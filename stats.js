f861b5a39bb752e6f8bcdf7fe7eb8b26243e462b
Jeff Minard <github.com@jrm.cc>
2011-04-29 18:03:49 -0700

    Adding two new types of collected data: Raw and Averaged (|r and |a respectively)

--- stats.js
+++ stats.js
@@ -5,6 +5,8 @@ var dgram  = require('dgram')
 
 var counters = {};
 var timers = {};
+var raws = [];
+var averages = {};
 var debugInt, flushInt, server;
 
 config.configFile(process.argv[2], function (config, oldConfig) {
@@ -16,7 +18,12 @@ config.configFile(process.argv[2], function (config, oldConfig) {
   if (config.debug) {
     if (debugInt !== undefined) { clearInterval(debugInt); }
     debugInt = setInterval(function () { 
-      sys.log("Counters:\n" + sys.inspect(counters) + "\nTimers:\n" + sys.inspect(timers));
+      sys.log(
+         "Inspect::\nCounters:\n" + sys.inspect(counters) 
+       + "\nTimers:\n" + sys.inspect(timers)
+       + "\nRaws:\n" + sys.inspect(raws)
+       + "\nAverages:\n" + sys.inspect(averages)
+      );
     }, config.debugInterval || 10000);
   }
 
@@ -45,6 +52,13 @@ config.configFile(process.argv[2], function (config, oldConfig) {
             timers[key] = [];
           }
           timers[key].push(Number(fields[0] || 0));
+        } else if (fields[1].trim() == "r") {
+          raws.push([key, Number(fields[0] || 0), Math.round(new Date().getTime()/1000)]);
+        } else if (fields[1].trim() == "a") {
+          if (! averages[key]) {
+            averages[key] = [];
+          }
+          averages[key].push(Number(fields[0] || 0));
         } else {
           if (fields[2] && fields[2].match(/^@([\d\.]+)/)) {
             sampleRate = Number(fields[2].match(/^@([\d\.]+)/)[1]);
@@ -75,6 +89,27 @@ config.configFile(process.argv[2], function (config, oldConfig) {
         counters[key] = 0;
 
         numStats += 1;
+      }
+
+      for (idx in raws) {
+        statString += 'stats.' + raws[idx][0] + ' ' + raws[idx][1] + ' ' + raws[idx][2] + "\n";
+        numStats += 1;
+      }
+      raws = [];
+
+      for (key in averages) {
+        var vals = averages[key],
+            valCount = averages[key].length,
+            valTotal = 0;
+        if (vals.length >= 1) {
+          for (idx in vals) {
+            valTotal += vals[idx];
+          }
+          var averageVal = valTotal / valCount;
+          averages[key] = [];
+          statString += 'stats.' + key + ' ' + averageVal + ' ' + ts + "\n";
+          numStats += 1;
+        }
       }
 
       for (key in timers) {
