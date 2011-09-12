d76a92d8d639eeb16cd1ccdd2d953d65b1e9317a
Andrew Gwozdziewycz <git@apgwoz.com>
2011-09-12 10:53:34 -0400

    * multiple flush bucket support

--- exampleConfig.js
+++ exampleConfig.js
@@ -2,5 +2,11 @@
   graphitePort: 2003
 , graphiteHost: "graphite.host.com"
 , port: 8125
+, flushBuckets: [
+    {
+      pattern: "^.*"
+    , flushInterval: 10000
+    , statPrefix: "stats"
+    }
+  ]
 }
-
--- stats.js
+++ stats.js
@@ -3,14 +3,42 @@ var dgram  = require('dgram')
   , net    = require('net')
   , config = require('./config')
 
-var counters = {};
-var timers = {};
-var debugInt, flushInt, server;
+var counters = [];
+var timers = [];
+var debugInt, flushInts = [], server;
+
+
+String.prototype.startsWith = function(str) {
+  var l = str.length;
+  return this.substring(0, l) == str; 
+};
+
+String.prototype.endsWith = function(str) {
+  var l = str.length;
+  return this.substring(this.length - l) == str; 
+};
+
+String.prototype.removePrefix = function(str) {
+  if (this.startsWith(str)) {
+    return this.substring(str.length);
+  }
+  return this;
+}
 
 config.configFile(process.argv[2], function (config, oldConfig) {
   if (! config.debug && debugInt) {
     clearInterval(debugInt); 
     debugInt = false;
+  }
+                    
+  if (! config.flushBuckets) {
+    config.flushBuckets = [
+      {
+        pattern: ".*"
+      , flushInterval: 10000 
+      , statPrefix: "stats"
+      }
+    ];
   }
 
   if (config.debug) {
@@ -21,6 +49,12 @@ config.configFile(process.argv[2], function (config, oldConfig) {
   }
 
   if (server === undefined) {
+    // create the data buckets
+    for (var i = 0; i < config.flushBuckets.length; i++) {
+      counters.push({});
+      timers.push({});
+    }
+
     server = dgram.createSocket('udp4', function (msg, rinfo) {
       if (config.dumpMessages) { sys.log(msg.toString()); }
       var bits = msg.toString().split(':');
@@ -28,6 +62,20 @@ config.configFile(process.argv[2], function (config, oldConfig) {
                     .replace(/\s+/g, '_')
                     .replace(/\//g, '-')
                     .replace(/[^a-zA-Z_\-0-9\.]/g, '');
+      var flushBucket = -1;    
+                              
+      // find the appropriate bucket
+      for (var i = 0; i < config.flushBuckets.length; i++) {
+        if (key.match(config.flushBuckets[i].pattern || ".*")) {
+          flushBucket = i;
+          break;
+        }
+      }
+                                  
+      if (flushBucket < 0) {
+        sys.log("key " + key + "didn't match expected pattern\n");
+        return;
+      }
 
       if (bits.length == 0) {
         bits.push("1");
@@ -41,103 +89,103 @@ config.configFile(process.argv[2], function (config, oldConfig) {
             continue;
         }
         if (fields[1].trim() == "ms") {
-          if (! timers[key]) {
-            timers[key] = [];
+          if (! timers[flushBucket][key]) {
+            timers[flushBucket][key] = [];
           }
-          timers[key].push(Number(fields[0] || 0));
+          timers[flushBucket][key].push(Number(fields[0] || 0));
         } else {
           if (fields[2] && fields[2].match(/^@([\d\.]+)/)) {
             sampleRate = Number(fields[2].match(/^@([\d\.]+)/)[1]);
           }
-          if (! counters[key]) {
-            counters[key] = 0;
+          if (! counters[flushBucket][key]) {
+            counters[flushBucket][key] = 0;
           }
-          counters[key] += Number(fields[0] || 1) * (1 / sampleRate);
+          counters[flushBucket][key] += Number(fields[0] || 1) * (1 / sampleRate);
         }
       }
     });
 
     server.bind(config.port || 8125);
 
-    var flushInterval = Number(config.flushInterval || 10000);
+    var mkFlusher = function (flushBucket, flushInterval, statPrefix) {
+      return function() {
+        var statString = '';
+        var ts = Math.round(new Date().getTime() / 1000);
+        var numStats = 0;
+        var key, tmpKey;
 
-    flushInt = setInterval(function () {
-      var statString = '';
-      var ts = Math.round(new Date().getTime() / 1000);
-      var numStats = 0;
-      var key;
+        for (key in counters[flushBucket]) {
+          var value = counters[flushBucket][key] / (flushInterval / 1000);
 
-      for (key in counters) {
-        var value = counters[key] / (flushInterval / 1000);
-        var message = 'stats.' + key + ' ' + value + ' ' + ts + "\n";
-        message += 'stats_counts.' + key + ' ' + counters[key] + ' ' + ts + "\n";
-        statString += message;
-        counters[key] = 0;
+          tmpKey = key.removePrefix(statPrefix).removePrefix('.');
 
-        numStats += 1;
-      }
-
-      for (key in timers) {
-        if (timers[key].length > 0) {
-          var pctThreshold = config.percentThreshold || 90;
-          var values = timers[key].sort(function (a,b) { return a-b; });
-          var count = values.length;
-          var min = values[0];
-          var max = values[count - 1];
-
-          var mean = min;
-          var maxAtThreshold = max;
-
-          if (count > 1) {
-            var thresholdIndex = Math.round(((100 - pctThreshold) / 100) * count);
-            var numInThreshold = count - thresholdIndex;
-            values = values.slice(0, numInThreshold);
-            maxAtThreshold = values[numInThreshold - 1];
-
-            // average the remaining timings
-            var sum = 0;
-            for (var i = 0; i < numInThreshold; i++) {
-              sum += values[i];
-            }
-
-            mean = sum / numInThreshold;
-          }
-
-          timers[key] = [];
-
-          var message = "";
-          message += 'stats.timers.' + key + '.mean ' + mean + ' ' + ts + "\n";
-          message += 'stats.timers.' + key + '.upper ' + max + ' ' + ts + "\n";
-          message += 'stats.timers.' + key + '.upper_' + pctThreshold + ' ' + maxAtThreshold + ' ' + ts + "\n";
-          message += 'stats.timers.' + key + '.lower ' + min + ' ' + ts + "\n";
-          message += 'stats.timers.' + key + '.count ' + count + ' ' + ts + "\n";
-          statString += message;
-
+          statString += statPrefix + '.' + tmpKey + ' ' + value + ' ' + ts + "\n";
+          counters[flushBucket][key] = 0;
           numStats += 1;
         }
-      }
 
-      statString += 'statsd.numStats ' + numStats + ' ' + ts + "\n";
-      
-      try {
-        var graphite = net.createConnection(config.graphitePort, config.graphiteHost);
-        graphite.addListener('error', function(connectionException){
-          if (config.debug) {
-            sys.log(connectionException);
+        for (key in timers[flushBucket]) {
+          if (timers[flushBucket][key].length > 0) {
+            var pctThreshold = config.percentThreshold || 90;
+            var values = timers[flushBucket][key].sort(function (a,b) { return a-b; });
+            var count = values.length;
+            var min = values[0];
+            var max = values[count - 1];
+
+            var mean = min;
+            var maxAtThreshold = max;
+
+            if (count > 1) {
+              var thresholdIndex = Math.round(((100 - pctThreshold) / 100) * count);
+              var numInThreshold = count - thresholdIndex;
+              values = values.slice(0, numInThreshold);
+              maxAtThreshold = values[numInThreshold - 1];
+
+              // average the remaining timings
+              var sum = 0;
+              for (var i = 0; i < numInThreshold; i++) {
+                sum += values[i];
+              }
+
+              mean = sum / numInThreshold;
+            }
+
+            timers[flushBucket][key] = [];
+
+            var message = "";
+
+            tmpKey = key.removePrefix(statPrefix).removePrefix('.');
+
+            message += statPrefix + '.timers.' + tmpKey + '.mean ' + mean + ' ' + ts + "\n";
+            message += statPrefix + '.timers.' + tmpKey + '.upper ' + max + ' ' + ts + "\n";
+            message += statPrefix + '.timers.' + tmpKey + '.upper_' + pctThreshold + ' ' + maxAtThreshold + ' ' + ts + "\n";
+            message += statPrefix + '.timers.' + tmpKey + '.lower ' + min + ' ' + ts + "\n";
+            message += statPrefix + '.timers.' + tmpKey + '.count ' + count + ' ' + ts + "\n";
+
+            statString += message;
+
+            numStats += 1;
           }
-        });
-        graphite.on('connect', function() {
-          this.write(statString);
-          this.end();
-        });
-      } catch(e){
-        if (config.debug) {
-          sys.log(e);
         }
-      }
 
-    }, flushInterval);
+        statString += 'statsd.numStats ' + numStats + ' ' + ts + "\n";
+        var graphite = net.createConnection(config.graphitePort, config.graphiteHost);
+
+        graphite.on('connect', function() {
+                      this.write(statString);
+                      this.end();
+                    });
+      };
+    };
+    
+    // setup flushers
+    for (var i = 0; i < config.flushBuckets.length; i++) {
+      var flushInterval = new Number(config.flushBuckets[i].flushInterval || 10000);
+      flushInts.push(setInterval(mkFlusher(i
+                                         , flushInterval
+                                         , config.flushBuckets[i].statPrefix)
+                               , flushInterval));
+    }
   }
-
 });
 
