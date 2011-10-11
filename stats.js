b7b5c175fe558bf15c69bb72220652ce4d62dd7f
Marcus Barczak <mbarczak@etsy.com>
2011-10-11 11:34:14 -0400

    added a little tcp management interface for pulling stats from running statsd instance

--- stats.js
+++ stats.js
@@ -5,7 +5,19 @@ var dgram  = require('dgram')
 
 var counters = {};
 var timers = {};
-var debugInt, flushInt, server;
+var debugInt, flushInt, server, mgmt_server;
+var startup_time = Math.round(new Date().getTime() / 1000);
+
+var stats = {
+  graphite: {
+    last_flush: startup_time,
+    last_exception: startup_time
+  },
+  messages: {
+    last_msg_seen: startup_time,
+    bad_lines_seen: 0,
+  }
+};
 
 config.configFile(process.argv[2], function (config, oldConfig) {
   if (! config.debug && debugInt) {
@@ -38,6 +50,7 @@ config.configFile(process.argv[2], function (config, oldConfig) {
         var fields = bits[i].split("|");
         if (fields[1] === undefined) {
             sys.log('Bad line: ' + fields);
+            stats['messages']['bad_lines_seen']++;
             continue;
         }
         if (fields[1].trim() == "ms") {
@@ -55,9 +68,68 @@ config.configFile(process.argv[2], function (config, oldConfig) {
           counters[key] += Number(fields[0] || 1) * (1 / sampleRate);
         }
       }
+
+      stats['messages']['last_msg_seen'] = Math.round(new Date().getTime() / 1000);
+    });
+
+    mgmt_server = net.createServer(function(stream) {
+      stream.setEncoding('ascii');
+
+      stream.on('data', function(data) {
+        var cmd = data.trim();
+
+        switch(cmd) {
+          case "help":
+            stream.write("Commands: stats, counters, timers, quit\n\n");
+            break;
+
+          case "stats":
+            var now    = Math.round(new Date().getTime() / 1000);
+            var uptime = now - startup_time;
+
+            stream.write("uptime: " + uptime + "\n");
+
+            for (group in stats) {
+              for (metric in stats[group]) {
+                var val;
+
+                if (metric.match("^last_")) {
+                  val = now - stats[group][metric];
+                }
+                else {
+                  val = stats[group][metric];
+                }
+
+                stream.write(group + "." + metric + ": " + val + "\n");
+              }
+            }
+            stream.write("END\n\n");
+            break;
+
+          case "counters":
+            stream.write(sys.inspect(counters) + "\n");
+            stream.write("END\n\n");
+            break;
+
+          case "timers":
+            stream.write(sys.inspect(timers) + "\n");
+            stream.write("END\n\n");
+            break;
+
+          case "quit":
+            stream.end();
+            break;
+
+          default:
+            stream.write("ERROR\n");
+            break;
+        }
+
+      });
     });
 
     server.bind(config.port || 8125);
+    mgmt_server.listen(config.mgmt_port || 8126);
 
     var flushInterval = Number(config.flushInterval || 10000);
 
@@ -129,11 +201,13 @@ config.configFile(process.argv[2], function (config, oldConfig) {
         graphite.on('connect', function() {
           this.write(statString);
           this.end();
+          stats['graphite']['last_flush'] = Math.round(new Date().getTime() / 1000);
         });
       } catch(e){
         if (config.debug) {
           sys.log(e);
         }
+        stats['graphite']['last_execption'] = Math.round(new Date().getTime() / 1000);
       }
 
     }, flushInterval);
