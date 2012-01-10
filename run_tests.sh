766876b072122b3eccebaa2410079fe0d8bdef08
Mike Stipicevic <stipredirect@gmail.com>
2012-01-10 07:03:49 -0800

    Add basic test framework
    Adds a node-unit based test framework designed to test a running statsd
    instance and test its output. Should facilitate later testing of
    features and prevent regressions.

---
+++ run_tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+try {
+    var reporter = require('nodeunit').reporters.default;
+}
+catch(e) {
+    console.log("Cannot find nodeunit module.");
+    console.log("Make sure to run 'npm install nodeunit'");
+    process.exit();
+}
+
+process.chdir(__dirname);
+reporter.run(['test/']);
