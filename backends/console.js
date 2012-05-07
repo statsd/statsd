e0cf48e522367449cddeafb2b129607854879e6f
Mikito Takada <mikito.takada@gmail.com>
2012-05-07 14:11:15 -0700

    Add console backend

---
+++ backends/console.js
@@ -0,0 +1,45 @@
+var util = require('util');
+
+function ConsoleBackend(startupTime, config, emitter){
+  var self = this;
+  this.lastFlush = startupTime;
+  this.lastException = startupTime;
+
+  this.statsCache = {
+    counters: {},
+    gauges: {},
+    timers: {}
+  };
+
+  // attach
+  emitter.on('flush', function(timestamp, metrics) { self.flush(timestamp, metrics); });
+  emitter.on('status', function(callback) { self.status(callback); });
+};
+
+ConsoleBackend.prototype.flush = function(timestamp, metrics) {
+  var self = this;
+  console.log('Flushing stats at', new Date(timestamp * 1000).toString());
+
+  // merge with previously sent values
+  Object.keys(self.statsCache).forEach(function(type) {
+    if(!metrics[type]) return;
+    Object.keys(metrics[type]).forEach(function(name) {
+      var value = metrics[type][name];
+      self.statsCache[type][name] || (self.statsCache[type][name] = 0);
+      self.statsCache[type][name] += value;
+    });
+  });
+
+  console.log(util.inspect(self.statsCache, false, 5, true));
+};
+
+ConsoleBackend.prototype.status = function(write) {
+  ['lastFlush', 'lastException'].forEach(function(key) {
+    write(null, 'console', key, this[key]);
+  }, this);
+};
+
+exports.init = function(startupTime, config, events) {
+  var instance = new ConsoleBackend(startupTime, config, events);
+  return true;
+};
