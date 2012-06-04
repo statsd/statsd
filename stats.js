01bfd038e6d59d57c8ae60acd03b8a75c37838b2
Phil Sorber <phil@omniti.com>
2012-06-04 13:15:33 -0400

    Fixing white space in a separate commit.

--- stats.js
+++ stats.js
@@ -90,47 +90,47 @@ config.configFile(process.argv[2], function (config, oldConfig) {
       for (midx in metrics) {
         if (config.dumpMessages) { util.log(metrics[midx].toString()); }
         var bits = metrics[midx].toString().split(':');
-      var key = bits.shift()
-                    .replace(/\s+/g, '_')
-                    .replace(/\//g, '-')
-                    .replace(/[^a-zA-Z_\-0-9\.]/g, '');
+        var key = bits.shift()
+                      .replace(/\s+/g, '_')
+                      .replace(/\//g, '-')
+                      .replace(/[^a-zA-Z_\-0-9\.]/g, '');
 
-      if (keyFlushInterval > 0) {
-        if (! keyCounter[key]) {
-          keyCounter[key] = 0;
+        if (keyFlushInterval > 0) {
+          if (! keyCounter[key]) {
+            keyCounter[key] = 0;
+          }
+          keyCounter[key] += 1;
         }
-        keyCounter[key] += 1;
-      }
 
-      if (bits.length == 0) {
-        bits.push("1");
-      }
+        if (bits.length == 0) {
+          bits.push("1");
+        }
 
-      for (var i = 0; i < bits.length; i++) {
-        var sampleRate = 1;
-        var fields = bits[i].split("|");
-        if (fields[1] === undefined) {
-            util.log('Bad line: ' + fields);
-            stats['messages']['bad_lines_seen']++;
-            continue;
+        for (var i = 0; i < bits.length; i++) {
+          var sampleRate = 1;
+          var fields = bits[i].split("|");
+          if (fields[1] === undefined) {
+              util.log('Bad line: ' + fields);
+              stats['messages']['bad_lines_seen']++;
+              continue;
+          }
+          if (fields[1].trim() == "ms") {
+            if (! timers[key]) {
+              timers[key] = [];
+            }
+            timers[key].push(Number(fields[0] || 0));
+          } else if (fields[1].trim() == "g") {
+            gauges[key] = Number(fields[0] || 0);
+          } else {
+            if (fields[2] && fields[2].match(/^@([\d\.]+)/)) {
+              sampleRate = Number(fields[2].match(/^@([\d\.]+)/)[1]);
+            }
+            if (! counters[key]) {
+              counters[key] = 0;
+            }
+            counters[key] += Number(fields[0] || 1) * (1 / sampleRate);
+          }
         }
-        if (fields[1].trim() == "ms") {
-          if (! timers[key]) {
-            timers[key] = [];
-          }
-          timers[key].push(Number(fields[0] || 0));
-        } else if (fields[1].trim() == "g") {
-          gauges[key] = Number(fields[0] || 0);
-        } else {
-          if (fields[2] && fields[2].match(/^@([\d\.]+)/)) {
-            sampleRate = Number(fields[2].match(/^@([\d\.]+)/)[1]);
-          }
-          if (! counters[key]) {
-            counters[key] = 0;
-          }
-          counters[key] += Number(fields[0] || 1) * (1 / sampleRate);
-        }
-      }
       }
 
       stats['messages']['last_msg_seen'] = Math.round(new Date().getTime() / 1000);
