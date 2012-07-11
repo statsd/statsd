b56c30b9f7aafecb137690e78e5388cc3836a8c0
Matt Spitz <mspitz@ebay.com>
2012-07-11 17:58:19 -0400

    Using %d rather than %i in StatsdClient.java example.
    %i isn't a valid conversion type for String formatters: http://docs.oracle.com/javase/6/docs/api/java/util/Formatter.html

--- examples/StatsdClient.java
+++ examples/StatsdClient.java
@@ -147,7 +147,7 @@ public class StatsdClient {
 				return true;
 			} else {
 				log.error(String.format(
-						"Could not send entirely stat %s to host %s:%d. Only sent %i bytes out of %i bytes", stat,
+						"Could not send entirely stat %s to host %s:%d. Only sent %d bytes out of %d bytes", stat,
 						_address.getHostName(), _address.getPort(), nbSentBytes, data.length));
 				return false;
 			}
