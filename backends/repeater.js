9dfb16d08f2ddce0edae7685eba2d05b29f93ee9
allevo <tomallevi@gmail.com>
2014-07-17 23:39:04 +0200

    Avoid global variables

--- backends/repeater.js
+++ backends/repeater.js
@@ -6,19 +6,17 @@ var util = require('util')
 
 var BackendBase = require('./base');
 
-var l;
-var debug;
 
-function RepeaterBackend(startupTime, config, emitter) {
-	BackendBase.call(this, startupTime, config.repeater || [], emitter);
+function RepeaterBackend(startupTime, config, emitter, logger) {
+	BackendBase.call(this, startupTime, config, emitter, logger);
 
   this.sock = (config.repeaterProtocol == 'udp6') ?
         dgram.createSocket('udp6') :
         dgram.createSocket('udp4');
   // Attach DNS error handler
   this.sock.on('error', function (err) {
-    if (debug) {
-      l.log('Repeater error: ' + err);
+    if (config.debug) {
+      logger.log('Repeater error: ' + err);
     }
   });
 }
@@ -26,20 +24,17 @@ util.inherits(RepeaterBackend, BackendBase);
 
 RepeaterBackend.prototype.onPacketEvent = function(packet) {
   var self = this;
-  var hosts = self.config;
+  var hosts = self.config.repeater;
   for(var i=0; i<hosts.length; i++) {
-    self.sock.send(packet,0,packet.length,hosts[i].port,hosts[i].host,
-                   function(err) {
-      if (err && debug) {
-        l.log(err);
+    self.sock.send(packet, 0, packet.length, hosts[i].port, hosts[i].host, function(err) {
+      if (err && self.config.debug) {
+        self.logger.log(err);
       }
     });
   }
 };
 
 exports.init = function(startupTime, config, events, logger) {
-  new RepeaterBackend(startupTime, config, events);
-  debug = config.debug;
-  l = logger;
+  new RepeaterBackend(startupTime, config, events, logger);
   return true;
 };
