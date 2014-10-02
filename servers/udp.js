4d129f8186b35a330f5b9a18d8ffeb830aee90de
Sam Roberts <sam@strongloop.com>
2014-10-02 14:12:53 -0700

    udp: add support for ephemeral port and ipc

--- servers/udp.js
+++ servers/udp.js
@@ -1,8 +1,40 @@
 var dgram  = require('dgram');
+var control = require('strong-control-channel/process');
+var server;
+
+try {
+  control.attach(onRequest);
+  console.log('attach', !!process.send)
+} catch(er) {
+  console.error('Not initializing IPC control channel');
+}
+
+function onRequest(req, callback) {
+  console.log('req', req);
+  switch (req.cmd) {
+    case 'address': {
+      if (!server)
+        return callback({error: 'notready'});
+      return callback({
+        address: server.address(),
+      });
+    }
+    default:
+      return callback({error: 'unsupported'});
+  }
+}
 
 exports.start = function(config, callback){
   var udp_version = config.address_ipv6 ? 'udp6' : 'udp4';
-  var server = dgram.createSocket(udp_version, callback);
-  server.bind(config.port || 8125, config.address || undefined);
+  var port = 'port' in config ? config.port : 8125;
+  server = dgram.createSocket(udp_version, callback);
+  server.bind(port, config.address || undefined);
+
+  server.on('listening', function() {
+    var addr = this.address();
+    console.log('Server udp listening with %s on %s:%d',
+      addr.family, addr.address, addr.port);
+  });
+
   return true;
 };
