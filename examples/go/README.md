23fd7a38bdc35b6dd328b7c0fbdb09702a564c45
Sam Roberts <sam@strongloop.com>
2014-11-18 15:23:27 -0800

    Strip unused code

--- examples/go/README.md
+++
@@ -1,6 +0,0 @@
-StatsD Go client
-================
-
-This is a StatsD client written in Go.
-
-View the project's documentation on godoc: [http://godoc.org/github.com/etsy/statsd/examples/go](http://godoc.org/github.com/etsy/statsd/examples/go)
--- stats.js
+++ stats.js
@@ -1,25 +1,8 @@
-/*jshint node:true, laxcomma:true */
-
-// Exit on loss of parent process, if it had established an ipc control
-// channel.  We do this ASAP because we don't want child processes to leak,
-// outliving their parent. Note that if the parent has not established an
-// 'ipc' channel to us, this will be a no-op, the disconnect event will never
-// occur.
-process.on('disconnect', function() {
-  process.exit(2);
-});
-
-var util    = require('util')
-  , net    = require('net')
-  , config = require('./lib/config')
-  , helpers = require('./lib/helpers')
-  , fs     = require('fs')
-  , events = require('events')
-  , logger = require('./lib/logger')
-  , set = require('./lib/set')
-  , pm = require('./lib/process_metrics')
-  , process_mgmt = require('./lib/process_mgmt')
-  , mgmt = require('./lib/mgmt_console');
+var helpers = require('./lib/helpers')
+var events = require('events')
+var logger = require('./lib/logger')
+var set = require('./lib/set')
+var pm = require('./lib/process_metrics')
 
 
 // initialize data structures with defaults for statsd stats
@@ -167,15 +150,10 @@ var stats = {
   }
 };
 
-// Global for the logger
-var l;
+var l; // XXX remove
 
-config.configFile(process.argv[2], function (config) {
+exports.start = function start (config) {
   conf = config;
-
-  process_mgmt.init(config);
-
-  l = new logger.Logger(config.log || {});
 
   // setup config for stats prefix
   var prefixStats = config.prefixStats;
@@ -188,8 +166,6 @@ config.configFile(process.argv[2], function (config) {
   //now set to zero so we can increment them
   counters[bad_lines_seen]   = 0;
   counters[packets_received] = 0;
-
-  if (!serverLoaded) {
 
     // key counting
     var keyFlushInterval = Number((config.keyFlush && config.keyFlush.interval) || 0);
@@ -274,123 +250,6 @@ config.configFile(process.argv[2], function (config) {
       stats.messages.last_msg_seen = Math.round(new Date().getTime() / 1000);
     });
 
-    mgmtServer = net.createServer(function(stream) {
-      stream.setEncoding('ascii');
-
-      stream.on('error', function(err) {
-        l.log('Caught ' + err +', Moving on');
-      });
-
-      stream.on('data', function(data) {
-        var cmdline = data.trim().split(" ");
-        var cmd = cmdline.shift();
-
-        switch(cmd) {
-          case "help":
-            stream.write("Commands: stats, counters, timers, gauges, delcounters, deltimers, delgauges, health, quit\n\n");
-            break;
-
-          case "health":
-            if (cmdline.length > 0) {
-              var cmdaction = cmdline[0].toLowerCase();
-              if (cmdaction === 'up') {
-                healthStatus = 'up';
-              } else if (cmdaction === 'down') {
-                healthStatus = 'down';
-              }
-            }
-            stream.write("health: " + healthStatus + "\n");
-            break;
-
-          case "stats":
-            var now    = Math.round(new Date().getTime() / 1000);
-            var uptime = now - startup_time;
-
-            stream.write("uptime: " + uptime + "\n");
-
-            var stat_writer = function(group, metric, val) {
-              var delta;
-
-              if (metric.match("^last_")) {
-                delta = now - val;
-              }
-              else {
-                delta = val;
-              }
-
-              stream.write(group + "." + metric + ": " + delta + "\n");
-            };
-
-            // Loop through the base stats
-            for (var group in stats) {
-              for (var metric in stats[group]) {
-                stat_writer(group, metric, stats[group][metric]);
-              }
-            }
-
-            backendEvents.once('status', function(writeCb) {
-              stream.write("END\n\n");
-            });
-
-            // Let each backend contribute its status
-            backendEvents.emit('status', function(err, name, stat, val) {
-              if (err) {
-                l.log("Failed to read stats for backend " +
-                        name + ": " + err);
-              } else {
-                stat_writer(name, stat, val);
-              }
-            });
-
-            break;
-
-          case "counters":
-            stream.write(util.inspect(counters) + "\n");
-            stream.write("END\n\n");
-            break;
-
-          case "timers":
-            stream.write(util.inspect(timers) + "\n");
-            stream.write("END\n\n");
-            break;
-
-          case "gauges":
-            stream.write(util.inspect(gauges) + "\n");
-            stream.write("END\n\n");
-            break;
-
-          case "delcounters":
-            mgmt.delete_stats(counters, cmdline, stream);
-            break;
-
-          case "deltimers":
-            mgmt.delete_stats(timers, cmdline, stream);
-            break;
-
-          case "delgauges":
-            mgmt.delete_stats(gauges, cmdline, stream);
-            break;
-
-          case "quit":
-            stream.end();
-            break;
-
-          default:
-            stream.write("ERROR\n");
-            break;
-        }
-
-      });
-    });
-
-    if ('mgmt_port' in config) {
-      mgmtServer.listen(config.mgmt_port || 8126, config.mgmt_address || undefined);
-    }
-
-    if (config.debug) {
-      util.log("server is up");
-    }
-
     pctThreshold = config.percentThreshold || 90;
     if (!Array.isArray(pctThreshold)) {
       pctThreshold = [ pctThreshold ]; // listify percentiles so single values work the same
@@ -403,46 +262,10 @@ config.configFile(process.argv[2], function (config) {
       for (var i = 0; i < config.backends.length; i++) {
         loadBackend(config, config.backends[i]);
       }
-    } else {
-      // The default backend is graphite
-      loadBackend(config, './backends/graphite');
     }
 
     // Setup the flush timer
     var flushInt = setInterval(flushMetrics, flushInterval);
 
-    if (keyFlushInterval > 0) {
-      var keyFlushPercent = Number((config.keyFlush && config.keyFlush.percent) || 100);
-      var keyFlushLog = config.keyFlush && config.keyFlush.log;
-
-      keyFlushInt = setInterval(function () {
-        var sortedKeys = [];
-
-        for (var key in keyCounter) {
-          sortedKeys.push([key, keyCounter[key]]);
-        }
-
-        sortedKeys.sort(function(a, b) { return b[1] - a[1]; });
-
-        var logMessage = "";
-        var timeString = (new Date()) + "";
-
-        // only show the top "keyFlushPercent" keys
-        for (var i = 0, e = sortedKeys.length * (keyFlushPercent / 100); i < e; i++) {
-          logMessage += timeString + " count=" + sortedKeys[i][1] + " key=" + sortedKeys[i][0] + "\n";
-        }
-
-        if (keyFlushLog) {
-          var logFile = fs.createWriteStream(keyFlushLog, {flags: 'a+'});
-          logFile.write(logMessage);
-          logFile.end();
-        } else {
-          process.stdout.write(logMessage);
-        }
-
-        // clear the counter
-        keyCounter = {};
-      }, keyFlushInterval);
-    }
-  }
-});
+    // XXX unref()
+}
