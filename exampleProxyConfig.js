0a20119ad8c75f81dde7916b3b25e56027f21ec3
Tyler Nisonoff <tyler@asana.com>
2016-02-12 18:04:29 -0800

    Allow the proxy to listen over TCP

--- exampleProxyConfig.js
+++ exampleProxyConfig.js
@@ -15,6 +15,12 @@ Optional Variables:
   checkInterval:    health status check interval [default: 10000]
   cacheSize:        size of the cache to store for hashring key lookups [default: 10000]
   forkCount:        number of child processes (cluster module), number or 'auto' for utilize all cpus [default:0]
+  server:           the server to load. The server must exist by name in the directory
+                    servers/. If not specified, the default udp server will be loaded.
+                    Note: This will still send to the backends via udp regardless of the
+                    server type for the proxy
+                    * example for tcp server:
+                    "./servers/tcp"
 
 */
 {
@@ -23,6 +29,7 @@ nodes: [
 {host: '127.0.0.1', port: 8129, adminport: 8130},
 {host: '127.0.0.1', port: 8131, adminport: 8132}
 ],
+server: './servers/udp',
 udp_version: 'udp4',
 host:  '0.0.0.0',
 port: 8125,
