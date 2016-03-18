dd0b7ee2958f764a18a604010fa5f33819bc8015
Patrick Koch <pkoch@etsy.com>
2016-03-18 18:26:40 +0000

    proxy now uses config.address_ipv6 for ip version configuration related to pr #566

--- exampleProxyConfig.js
+++ exampleProxyConfig.js
@@ -10,8 +10,8 @@ Required Variables:
 
 Optional Variables:
 
-  udp_version:      defines if the address is an IPv4 or IPv6 address ['udp4' or 'udp6', default: 'udp4']
   host:             address to listen on over UDP [default: 0.0.0.0]
+  address_ipv6:     defines if the listen address is an IPv4 or IPv6 address [true or false, default: false]
   checkInterval:    health status check interval [default: 10000]
   cacheSize:        size of the cache to store for hashring key lookups [default: 10000]
   forkCount:        number of child processes (cluster module), number or 'auto' for utilize all cpus [default:0]
@@ -30,7 +30,7 @@ nodes: [
 {host: '127.0.0.1', port: 8131, adminport: 8132}
 ],
 server: './servers/udp',
-udp_version: 'udp4',
+
 host:  '0.0.0.0',
 port: 8125,
 forkCount: 0,
